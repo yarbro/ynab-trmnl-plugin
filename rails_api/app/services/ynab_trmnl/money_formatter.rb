9703902737035ad4e902cd514a42e6d89173f604
David Yarbro <david@yarb.ro>
2025-06-24 17:42:46 -0500

    Remove old rails api app and cleanup

--- rails_api/app/services/ynab_trmnl/money_formatter.rb
+++
@@ -1,8 +0,0 @@
-module YnabTrmnl
-  module MoneyFormatter
-    def self.format(milliunits)
-      amount = milliunits.to_f / 1000
-      ActionController::Base.helpers.number_to_currency(amount)
-    end
-  end
-end
